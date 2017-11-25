eaada77b0f93738b5073cde40de7d3897286ed62
Nathan Clark Baumgartner <nbaumg1193@gmail.com>
2017-11-25 13:20:07 -0600

    react not rendering

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import 'whatwg-fetch';
-import Article from './Article'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import FlatButton from 'material-ui/FlatButton';
 import Dialog from 'material-ui/Dialog';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
-import MERN from '../assets/MERN.jpg';
 
 class App extends React.Component {
   constructor(props) {
